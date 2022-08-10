d70719462f47aabd63cdab50785ca01b95a564f3
Cleo <mz5318@ic.ac.uk>
2022-08-10 23:09:09 +0100

    version1

--- scripts.js
+++ scripts.js
@@ -2,3 +2,18 @@
 function populate_uniprot(uni){
     document.getElementById('input-uniprot').value=uni;
 }
+
+function toggleInfo() {
+    var x = document.getElementById("info-box");
+    if (x.style.visibility === "" || x.style.visibility === "hidden") {
+        x.style.visibility = "visible";
+    } else {
+        x.style.visibility = "hidden";
+    }
+}
+
+// Change color code accordingly
+var colcodes = document.getElementsByClassName("colcode")
+for (var i = 0; i < colcodes.length; i++) {
+    colcodes.item(i).style.color = colcodes.item(i).textContent
+}
